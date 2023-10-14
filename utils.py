b30e3ba7d28b759d3037c3d7c2db93f13a880819
Chris Harris <>
2023-10-14 16:19:33 +1030

    implemented TeamResult dict type

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
+from typing import TypedDict
 import yaml
 
+import pandas as pd
+
 CONFIG = {}
+
+class TeamResult(TypedDict):
+    team_number: int
+    result: pd.DataFrame
 
 # read config
 with open("config.yml", "r") as config_fp:
