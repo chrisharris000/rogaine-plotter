ddb09ab2d67837e457e3bc49ee292b18300c0f4c
Chris Harris <>
2023-10-14 16:27:21 +1030

    added description to TeamResult

--- utils.py
+++ utils.py
@@ -6,6 +6,15 @@ import pandas as pd
 CONFIG = {}
 
 class TeamResult(TypedDict):
+    """
+    key: team_number is the unique ID of the team
+    value: result is a dataframe with the following columns
+        idx - unique index of indicating order in which controls were visited, int
+        control - number of the control visited, int
+        cumulative_points, total points after arriving at the control, int
+        time_split, time taken to travel from the previous control to the current control, Timedelta
+        distance_travelled, straight-line distance travelled between previous and current control, float
+    """
     team_number: int
     result: pd.DataFrame
 
