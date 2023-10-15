abcdb651f9ebbb3ae163d1526325db053b467a10
Chris Harris <>
2023-10-15 19:43:48 +1030

    moved putText for stats to separate method

--- results_plotter/results_plotter.py
+++ results_plotter/results_plotter.py
@@ -24,18 +24,42 @@ class ResultsPlotter:
         cv2.namedWindow(stats_window_name, cv2.WINDOW_NORMAL)
 
         while True:
-            white_background = np.ones((500, 500, 3))
-            cv2.putText(white_background, str(time.time()), 
-                        (100,100),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=1,
-                        color=(255, 0, 0),
-                        thickness=2,
-                        lineType=2)
+            # start with blank canvas for stats
+            stats_width, stats_height = 750, 750
+            stats_background = np.ones((stats_width, stats_height, 3))
+
+            stats_background = self.add_stats_text(stats_background)
+
             cv2.imshow(map_window_name, self.map)
-            cv2.imshow(stats_window_name, white_background)
+            cv2.imshow(stats_window_name, stats_background)
 
             k = cv2.waitKey(1) & 0xFF
             if k == 27:
                 break
+
         cv2.destroyAllWindows()
+
+    def add_stats_text(self, stats_background) -> np.ndarray:
+        stats_font_settings = {
+            "fontFace": cv2.FONT_HERSHEY_SIMPLEX,
+            "fontScale": 1,
+            "color": (255, 0, 0),
+            "thickness": 2,
+            "lineType": 2
+        }
+
+        # stats page title
+        cv2.putText(stats_background, f"Stats for team {self.config['team_number']}", 
+                    (50, 50),
+                    **stats_font_settings
+                    )
+        
+        # event duration clock
+
+        # cumulative points
+
+        # leading team
+
+        # distance travelled
+
+        return stats_background
